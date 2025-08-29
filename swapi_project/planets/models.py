d386e467743d97ea140422951b0960fc97d820f8
adish-ct <adishathu525@gmail.com>
2025-08-29 17:41:28 +0530

    Feat(Schema): application configured and also database schema created

--- swapi_project/planets/models.py
+++ swapi_project/planets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Planet(models.Model):
+    name = models.CharField(max_length=100)
+    population = models.CharField(max_length=50)
+    climates = models.CharField(max_length=200)
+    terrains = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
