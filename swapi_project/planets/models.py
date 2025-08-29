a663937d21077099be15aa40dab8938e0e8857fa
adish-ct <adishathu525@gmail.com>
2025-08-29 17:58:11 +0530

    Fix(schema changed): added null and blank constraints to avoid potential issues

--- swapi_project/planets/models.py
+++ swapi_project/planets/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+# using external api to write entry on the table so allowing null and blank.
 class Planet(models.Model):
-    name = models.CharField(max_length=100)
-    population = models.CharField(max_length=50)
-    climates = models.CharField(max_length=200)
-    terrains = models.CharField(max_length=200)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    population = models.CharField(max_length=50, null=True, blank=True)
+    climates = models.CharField(max_length=200, null=True, blank=True)
+    terrains = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name
