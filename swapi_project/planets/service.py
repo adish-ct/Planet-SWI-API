8bdf7ecb6d0cffeec414c907ff766e386ab2987e
adish-ct <adishathu525@gmail.com>
2025-08-30 13:40:35 +0530

    Fix(api configuration): configured url and endpoints

--- swapi_project/planets/service.py
+++ swapi_project/planets/service.py
@@ -1,6 +1,7 @@
 import requests
 from .models import Planet
 
+
 def fetch_and_store():
     endpoint = "https://swapi-graphql.netlify.app/.netlify/functions/index"
     query = """
@@ -14,15 +15,25 @@ def fetch_and_store():
         }
       }
     }
-    """
-    response = requests.post(endpoint, json={'query': query})
-    data = response.json()
-    print(f"Response Data : {data}")
-    planets_list = data['data']['allPlanets']['planets']
-    for item in planets_list:
-        Planet.objects.create(
-            name=item.get('name'),
-            population=item.get('population') or 'unknown',
-            climates=", ".join(item.get('climates')),
-            terrains=", ".join(item.get('terrains'))
-        )
+    """.strip()  # Remove extra whitespace
+
+    headers = {'Content-Type': 'application/json'}
+    try:
+        response = requests.post(endpoint, json={'query': query}, headers=headers)
+        data = response.json()
+        print(f"Response Data : {data}")
+        # Defensive: check for 'data' key
+        if 'data' in data:
+            planets_list = data['data']['allPlanets']['planets']
+            for item in planets_list:
+                Planet.objects.create(
+                    name=item.get('name'),
+                    population=item.get('population') or 'unknown',
+                    climates=", ".join(item.get('climates', [])),
+                    terrains=", ".join(item.get('terrains', []))
+                )
+        else:
+            print("Query failed: ", data)
+    except Exception as e:
+        print(f"Exception Raised during API call : {e}\n")
+
